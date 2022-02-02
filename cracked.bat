4fe2b6489c3a4452737bbd26b877628a9451761d
Spinayy <47867642+Spinayy@users.noreply.github.com>
2022-02-02 20:54:56 +0200

    Create cracked.bat

---
+++ cracked.bat
@@ -0,0 +1,35 @@
+@shift /0
+@echo off >nul
+chcp 65001 >nul
+:again 
+cls
+color 3
+title PROJECT BEAR 
+echo Made by Ak47 Paid Version
+echo.
+goto main
+:main
+cls
+echo.
+echo made by ak 47 cracked
+echo    [1] Steam Acc Gen    [2] Free steam games [3] Stresser hub [4] Fivem Unban Method
+echo.
+echo    [5]Lmenu      [6]Anticheat          [7]Anonymous ping attack 
+echo.
+echo    [8]Ligma    [9] cheat engine       [0] 300 fiveM menus 
+:mainlogo
+set /p main=Choose your number:
+if %main% == 1 start https://accgen.cathook.club/
+if %main% == 2 start https://steamunlockedgames.com/
+if %main% == 3 start https://instant-stresser.com/
+if %main% == 4 start https://mega.nz/file/wYc2CBra#YdWo7osw6PJRTJQ9zwbXhrOC6Lfhf4BljZPXc_XG6Uc
+if %main% == 5 start https://mega.nz/file/hZFgXTqK#Tc9hKDLyKr6wYcqQyIl6aZWKmfbeSA7sPJlNyTFsMj0
+if %main% == 6 start https://mega.nz/file/ZU9GWCBB#rh3PnVqx176kprwDyGvqEWh4tYDUs9NTkBAwmyQommg
+if %main% == 7 start https://mega.nz/file/sUlBjKbJ#CQKpguc7_yX7rhg-P--kqv1FDQ-qZhkdMaw7ch7Y_pU
+if %main% == 8 start https://mega.nz/folder/RRsnTYCQ#YI21kyn1xzq8uEBctm_2ZA
+if %main% == 9 start https://cheatengine.org/
+if %main% == 0 start https://mega.nz/folder/YF0zWC7J#FVWnhX4e1AKTgc0fnlvuzw
+goto mainlogo
+:pinger
+start pinger.bat
+:goto mainlogo
